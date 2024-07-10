780e75c1270f709c50a5af2dcaba1da1cd026436
Bruno Sandoval <bruno.sandoval210@gmail.com>
2024-07-09 23:16:40 -0500

    se subio proyecto

--- front/src/app/app.component.spec.ts
+++ front/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'front-sistema-ecomerce' title`, () => {
+  it(`should have the 'front' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('front-sistema-ecomerce');
+    expect(app.title).toEqual('front');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, front-sistema-ecomerce');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, front');
   });
 });
