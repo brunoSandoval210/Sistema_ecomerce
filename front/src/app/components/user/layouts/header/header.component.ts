efc86d1c68edc373fb150112b04df30aafe72276
Bruno Sandoval <bruno.sandoval210@gmail.com>
2024-07-12 18:55:10 -0500

    avance slider y header

--- front/src/app/components/user/layouts/header/header.component.ts
+++ front/src/app/components/user/layouts/header/header.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SliderComponent } from '../slider/slider.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [SliderComponent,CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  showSlider=false;
 
+  toggleSlider(){
+    this.showSlider = !this.showSlider;
+    console.log("Slider toggled, current state:", this.showSlider);
+  }
 }
