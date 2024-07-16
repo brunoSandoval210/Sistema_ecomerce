81148774a03b970aea09266ae92ca98dd7fd4d09
Bruno Sandoval <bruno.sandoval210@gmail.com>
2024-07-16 18:27:43 -0500

    nueva actualizacion carousel

--- front/src/app/components/user/home/carrousel-sale/carrousel-sale.component.ts
+++ front/src/app/components/user/home/carrousel-sale/carrousel-sale.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import {
+  CarouselComponent,
+  CarouselControlComponent,
+  CarouselIndicatorsComponent,
+  CarouselInnerComponent,
+  CarouselItemComponent,
+  ThemeDirective
+} from '@coreui/angular';
 
 @Component({
   selector: 'app-carrousel-sale',
   standalone: true,
-  imports: [],
+  imports: [ThemeDirective, CarouselComponent, CarouselIndicatorsComponent, CarouselInnerComponent, NgFor, CarouselItemComponent, CarouselControlComponent, RouterLink],
   templateUrl: './carrousel-sale.component.html',
   styleUrl: './carrousel-sale.component.css'
+  
 })
-export class CarrouselSaleComponent {
+export class CarrouselSaleComponent implements OnInit {
+
+  constructor() { }
+  slides: any[] = new Array(3).fill({ id: -1, src: '', title: '', subtitle: '' });
+
+  ngOnInit(): void {
+    this.slides[0] = {
+      src: 'img/carrousel.png'
+    };
+    this.slides[1] = {
+      src: 'img/carrousel1.png'
+    };
+  }
 
 }
--- front/src/app/components/user/home/home.component.ts
+++ front/src/app/components/user/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CarrouselComponent } from './carrousel/carrousel.component';
+import { CarrouselSaleComponent } from './carrousel-sale/carrousel-sale.component';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CarrouselComponent],
+  imports: [CarrouselComponent,CarrouselSaleComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
