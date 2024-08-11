1ba71aec39f4cde22f7d2b4eae3e1be38fe3b55c
chrisstiandiaz <christian.mdiaz2002@gmal.com>
2024-08-10 23:22:19 -0500

    Commit de prueba 2

--- front/src/app/components/user/catalogue/catalogue.component.ts
+++ front/src/app/components/user/catalogue/catalogue.component.ts
@@ -30,7 +30,20 @@ export class CatalogueComponent implements OnInit {
   }
 
   onAddCart(product:Product) {
-    this.items = [... this.items, { product: {...product}, quantity: 1}]
+    const hasItem = this.items.find( item => item.product.id == product.id); // Este código busca los elementos/items
+    if(hasItem) {
+      this.items = this.items.map(item => {
+        if (item.product.id === product.id){
+          return{
+            ... item,
+            quantity: item.quantity + 1
+          }
+        }
+        return item;
+      })
+    }else {
+      this.items = [... this.items, { product: {...product}, quantity: 1}]
+    }
   }
 }
 
